33efb8922d35c230a42cc8de405ae129358352a0
perismuthoni <muthonimuriuki22@gmail.com>
2022-05-07 14:46:52 +0300

    routine commit

--- OnlineShopping/urls.py
+++ OnlineShopping/urls.py
@@ -6,4 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] +static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
